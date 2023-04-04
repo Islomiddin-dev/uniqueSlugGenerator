21d390c64df260e37921910005ccdd2b61003656
Islomiddin <hamisha0327@gmail.com>
2023-04-04 13:03:16 +0500

    servivice-provider

--- src/LibraryServiceProvider.php
+++ src/LibraryServiceProvider.php
@@ -8,6 +8,10 @@ class LibraryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->call(
+            'php artisan vendor:publish --provider="IslomiddinDev\UniqueSlugGenerator\LibraryServiceProvider"'
+        );
+
         $this->publishes([
             __DIR__ . '/Helpers' => app_path('Helpers'),
             __DIR__ . '/HelperServiceProvider.php' => app_path('Providers/HelperServiceProvider.php'),
