34bac7caa058718d9b361eafb747cc2c200ba74e
Islomiddin <hamisha0327@gmail.com>
2023-04-12 05:00:15 +0500

    fix

--- src/LibraryServiceProvider.php
+++ src/LibraryServiceProvider.php
@@ -8,9 +8,20 @@ class LibraryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/Helpers' => app_path('Helpers'),
-            __DIR__ . '/HelperServiceProvider.php' => app_path('Providers/HelperServiceProvider.php'),
-        ], 'unique-slug-generator');
+        $files = [];
+        if (file_exists(app_path('Helpers'))) {
+            $files[__DIR__ . '/Helpers/Slug.php'] = app_path('Helpers/Slug.php');
+        }
+
+        if (file_exists(app_path('Providers/HelperServiceProvider.php'))) {
+            echo 'HelperServiceProvider already exists. Please add this code to its register method.' . PHP_EOL;
+            echo 'foreach (glob(app_path(\'Helpers\') . \'/*.php\') as $file) {' . PHP_EOL;
+            echo '    require_once $file;' . PHP_EOL;
+            echo '}' . PHP_EOL;
+        } else {
+            $files[__DIR__ . '/HelperServiceProvider.php'] = app_path('Providers/HelperServiceProvider.php');
+        }
+
+        $this->publishes($files, 'unique-slug-generator');
     }
 }
