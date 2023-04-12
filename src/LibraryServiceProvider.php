97344a3e6cf75974e24071957674418e829c7ac9
Islomiddin <hamisha0327@gmail.com>
2023-04-12 06:10:13 +0500

    fix

--- src/LibraryServiceProvider.php
+++ src/LibraryServiceProvider.php
@@ -3,12 +3,14 @@
 namespace IslomiddinDev\UniqueSlugGenerator;
 
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class LibraryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         $message = "\033[32m Package islomiddin-dev/unique-slug-generator is successfully installed" . PHP_EOL . "Please add this provider to the list of providers in your config/app.php file." . PHP_EOL . "App\Providers\HelperServiceProvider::class,";
-        info($message);
+        $console = new ConsoleOutput();
+        $console->writeln('<info>' . $message . '</info>');
     }
 }
