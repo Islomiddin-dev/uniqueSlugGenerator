5d38fa3006b2ad13f1248c232d7104e852c920e6
Islomiddin <hamisha0327@gmail.com>
2023-04-12 06:19:00 +0500

    fix

--- src/LibraryServiceProvider.php
+++ src/LibraryServiceProvider.php
@@ -9,8 +9,9 @@ class LibraryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // INFO background color blue and text color white
-        $message = "\033[44;37m Package islomiddin-dev/unique-slug-generator is successfully installed" . PHP_EOL . "Please add this provider to the list of providers in your config/app.php file." . PHP_EOL . "App\Providers\HelperServiceProvider::class,";
+        $message = "\033[44;37m INFO";
+        $message .= "\033[0m";
+        $message .= "Package islomiddin-dev/unique-slug-generator is successfully installed" . PHP_EOL . "Please add this provider to the list of providers in your config/app.php file." . PHP_EOL . "App\Providers\HelperServiceProvider::class,";
         $console = new ConsoleOutput();
         $console->writeln($message);
     }
