44c6f2777633915e8ec48f148d2ecb4034e72ac2
Islomiddin <hamisha0327@gmail.com>
2023-04-13 10:12:40 +0500

    fix

--- src/Helpers/Slug.php
+++ src/Helpers/Slug.php
@@ -1,14 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 function slug($modelClass, $attribute, $name): string
 {
+    $tableName = (new $modelClass)->getTable();
+    $copyModelClass = $modelClass;
+
     $slug = Str::slug($name);
 
     $traits = class_uses($modelClass);
 
-    $modelClass = in_array('Illuminate\Database\Eloquent\SoftDeletes', $traits) ? $modelClass::withTrashed() : $modelClass::query();
+    $modelClass = in_array('Illuminate\Database\Eloquent\SoftDeletes', $traits) ? $modelClass::query()->withTrashed() : $modelClass::query();
 
     $max = $modelClass->where($attribute, $name)->latest('id')->first(['slug']);
 
@@ -19,9 +23,8 @@ function slug($modelClass, $attribute, $name): string
         $slug = $slug . '-' . ($lastNumber + 1);
     }
 
-    if ($modelClass->where('slug', $slug)->exists()) {
-        $slug = slug($modelClass, 'slug', $slug);
+    if (DB::table($tableName)->where('slug', $slug)->exists()) {
+        $slug = slug($copyModelClass, 'slug', $slug);
     }
-
     return $slug;
 }
